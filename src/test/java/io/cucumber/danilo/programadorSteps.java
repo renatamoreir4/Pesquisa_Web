813a5825dcab1145e89cd1483f29fb9a910d7216
Renata Moreira <renatamoreirac.c@hotmail.com>
2021-03-23 22:27:34 -0300

    Acessando pagina Web

---
+++ src/test/java/io/cucumber/danilo/programadorSteps.java
@@ -0,0 +1,51 @@
+package io.cucumber.danilo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class programadorSteps {
+
+	public WebDriver browser;
+	
+	public programadorSteps() {
+		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");		
+		
+        browser = new ChromeDriver();
+	}
+
+
+
+
+
+    @Quando("eu acesso o site do tornese um programador")
+    public void eu_acesso_o_site_do_tornese_um_programador() {
+        browser.get("https://www.torneseumprogramador.com.br/");
+    }
+
+    @Quando("digito no campo busca {string}")
+    public void digito_no_campo_busca(String string) {
+        WebElement input = browser.findElement( By.cssSelector("input[name='q']") );
+	    input.sendKeys(string);
+	}
+    
+
+    @Quando("clico no botao pesquisar")
+    public void clico_no_botao_pesquisar() {
+        WebElement input = browser.findElement( By.cssSelector(".btn-pesquisa") );
+	 	input.click();
+    }
+
+    @Entao("devo acessar a pagina com resultados de busca")
+    public void devo_acessar_a_pagina_com_resultados_de_busca() {
+        assertEquals(true , browser.findElements( By.cssSelector(".div-card-aulas") ).size() > 0);
+        browser.quit();
+    }
+
+}    
